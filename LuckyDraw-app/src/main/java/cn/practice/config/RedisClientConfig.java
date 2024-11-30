8ff2b9bd02ead3f65b42ab3d402d6972df63e560
yan <eugenescat@gmail.com>
2024-11-30 22:27:57 +0800

    strategy Armony impl and test

--- LuckyDraw-app/src/main/java/cn/practice/config/RedisClientConfig.java
+++ LuckyDraw-app/src/main/java/cn/practice/config/RedisClientConfig.java
@@ -1,4 +1,4 @@
-package cn.bugstack.config;
+package cn.practice.config;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
--- LuckyDraw-app/src/main/java/cn/practice/config/RedisClientConfigProperties.java
+++ LuckyDraw-app/src/main/java/cn/practice/config/RedisClientConfigProperties.java
@@ -1,4 +1,4 @@
-package cn.bugstack.config;
+package cn.practice.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
