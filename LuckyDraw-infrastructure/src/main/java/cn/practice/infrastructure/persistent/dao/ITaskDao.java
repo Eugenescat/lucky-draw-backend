35a7b5d7eeffc094d37b2b5aad37793084fa6f0c
yan <eugenescat@gmail.com>
2025-01-05 23:53:49 +0800

    feat17:database design

---
+++ LuckyDraw-infrastructure/src/main/java/cn/practice/infrastructure/persistent/dao/ITaskDao.java
@@ -0,0 +1,10 @@
+package cn.practice.infrastructure.persistent.dao;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 任务表，发送MQ
+ * @create 2024-04-03 15:57
+ */
+public interface ITaskDao {
+}
+
